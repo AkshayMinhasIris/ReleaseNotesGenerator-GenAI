6e2175d02738a24d619c6741e5aeef8b24fc1841
Akshay Minhas <akshay.minhas.iris@gs1.org>
2025-04-01 00:43:22 +0530

    code refactor

--- Program.cs
+++ Program.cs
@@ -1,11 +1,33 @@
+using ReleaseNotesGenerator;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog can check logs on output window
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()    
+    .CreateLogger();
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddAzureOpenAIClient(builder.Configuration);
+builder.Services.AddGitHubClient(builder.Configuration);
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add CORS services
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy =>
+        {
+            policy.WithOrigins("https://example.com") // Replace with your allowed origin(s)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
 
 var app = builder.Build();
 
@@ -19,7 +41,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.UseCors("AllowSpecificOrigin");
 app.MapControllers();
 
 app.Run();
